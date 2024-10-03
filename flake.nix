2eb30b529fc30fa47d6d83e08725dbcfc9e07312
Adrien Faure <adrien.faure@protonmail.com>
2024-10-03 15:07:24 +0200

    closer to my problem

--- flake.nix
+++ flake.nix
@@ -1,20 +1,26 @@
 {
-  inputs.nixpkgs = {
-    url = "github:NixOS/nixpkgs/23.11";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
+    nix2containerInput.url = "github:nlewo/nix2container";
+    nix2containerInput.inputs.nixpkgs.follows = "nixpkgs";
+    flakeUtils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs {
-        system = "x86_64-linux";
-      };
-    in
-    {
-      packages.x86_64-linux = {
+  outputs = { self, nixpkgs, flakeUtils, ... }:
+    flakeUtils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages = {
           lock = pkgs.writeShellScriptBin "lock" ''
             # Lock nixpkgs
+            echo tell me why ?
             echo ${self.rev or self.dirtyRev or "No revision available"}
           '';
         };
-    };
+      }
+    );
 }
